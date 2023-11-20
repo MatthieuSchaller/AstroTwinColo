2235c08f8b4f755c7c54ccbdf92a1415a07849ae
Matthieu Schaller <42518815+MatthieuSchaller@users.noreply.github.com>
2023-11-20 18:27:44 -0500

    Update submit.sh

--- Session_2/simulation/submit.sh
+++ Session_2/simulation/submit.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -l
 
-./swift --cosmology --self-gravity --power --threads=16 parameter_cosmo.yml
+# COMPILE SWIFT USING:
+# ./autogen.sh
+# ./configure --enable-fof
+# make -j 4
+
+# FOR PEOPLE WITH ANACONDA INSTALLED, RUN:
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/anaconda/lib/
+
+# CHANGE ./swift TO THE PATH TO THE ACTUAL EXECUTABLE
+./swift --cosmology --self-gravity --power --fof --threads=4 parameter_cosmo.yml
 
